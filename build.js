73b0e5c610faeb789600cf7230d06f398c162c80
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-13 14:24:44 +0700

    Update build.js

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const StyleDictionaryPackage = require('style-dictionary');
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
-    name: 'test',
+    name: 'css/variables',
     formatter: function (dictionary, config) {
       return `${this.selector} {
         ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
@@ -24,84 +24,6 @@ StyleDictionaryPackage.registerTransform({
     }
     });
 
-StyleDictionaryPackage.registerFormat({
-  name: 'typescript/accurate',
-  formatter: function(dictionary, config) {
-    /*
-      allProperties is an array containing all the matched
-      tokens based on the filter.
-    */
-    const { allProperties } = dictionary
-
-    /*
-      Set up an empty object to hold the final shape to pass
-      to the custom template.
-
-      After the allProperties.map(), props will look like this:
-      {
-        'component-button': {
-          padding: '16px',
-          'font-size': '16px',
-          'text-align': 'center',
-          primary: { 'background-color': '#e63c19', color: '#ffffff' },
-          secondary: { 'background-color': '#fad8d1', color: '#0000ff' }
-        }
-      }
-    */
-    const props = {}
-
-    // go through properties and structure final props object
-    allProperties.map(prop => {
-      /*
-        Extract the attributes object created by the 'attribute/cti'
-        transform and the transformed token value.
-      */
-      const { attributes, value } = prop
-
-      // extract attributes to build custom class and style rules
-      const { category, type, item, subitem } = attributes
-
-      // build main classname for .scss file
-      const classname = `${category}_${type}`
-
-      /*
-        Add to the props object if it doesn't already exist.
-        We run the check to see if the classname exists already as an
-        object property because in our case, `classname` will be the
-        same for each token object in allProperties because each token
-        is under the same category and type.
-      */
-      if (!props.hasOwnProperty(classname)) {
-        props[classname] = {}
-      }
-
-      /*
-        If the token object has a subitem, use the item as the subclass.
-        Run the same check to see if this particular subclass (item) has
-        been added yet.
-      */
-      if (subitem) {
-        if (!props[classname].hasOwnProperty(item)) {
-          props[classname][item] = {}
-        }
-
-        // add the subitem and value as final CSS rule
-        props[classname][item][subitem] = value
-      }
-      else {
-        // add the item as a CSS rule, not a subclass
-        props[classname][item] = value
-      }
-    })
-
-    /*
-      Pass the final `props` object to our custom template to render
-      the contents for the final button.scss file.
-    */
-    return template({ props })
-  }
-});
-
 function getStyleDictionaryConfig(theme) {
   return {
     "source": [
@@ -109,11 +31,11 @@ function getStyleDictionaryConfig(theme) {
     ],
     "platforms": {
       "web": {
-        "transformGroup": "css",
+        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
         "buildPath": `output/`,
         "files": [{
             "destination": `${theme}.css`,
-            "format": "typescript/accurate",
+            "format": "css/variables",
             "selector": `.${theme}-theme`
           }]
       }
