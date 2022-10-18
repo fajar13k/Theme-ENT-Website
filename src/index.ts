ac239cbc334ab308cb3e2ebbfff2c5a96a494852
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-10-18 22:36:46 +0700

    Update2

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import deepmerge from 'deepmerge'
 
 export * from './colors'
 export * from './spacers'
-//export * from './typography'
-//export * from './tokens/foundation'
-//export * from './theme-qip'
-//export * from './theme-lpe'
 export { deepmerge as merge }
