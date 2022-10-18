738287b19b1626aaffa389edb0f6e458d4f39e8f
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-10-18 11:27:39 +0700

    Test build

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import deepmerge from 'deepmerge'
-
-export * from './colors'
-export * from './spacers'
-export * from './typography'
-export * from './tokens/foundation'
-export * from './theme-qip'
-export * from './theme-lpe'
-export { deepmerge as merge }
