117c9714933af263b909d9133ac1e5c8ab5719cb
Fahri Rusliyadi <fahri.rusliyadi@gmail.com>
2022-10-26 16:18:47 +0700

    Add Card

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,6 +2,7 @@ import { ThemeTokens } from '../tokens';
 import { badges } from './badges';
 import { buttons } from './buttons';
 import { cards } from './cards';
+import { colorscard } from './colorscard';
 import { colors } from './colors';
 import { forms } from './forms';
 import { layout } from './layout';
@@ -16,6 +17,7 @@ export const makeThemeStyles = (tokens: ThemeTokens) => {
     badges: badges(tokens),
     buttons: buttons(tokens),
     cards: cards(tokens),
+    colorscard: colorscard(tokens),
     colors: colors(tokens),
     forms: forms(tokens),
     layout: layout(),
--- src/theme-oss.ts
+++ src/theme-oss.ts
@@ -1,5 +1,6 @@
 import { makeTheme } from '@theme-ui/css/utils';
+import { tokens as foundation } from './tokens/foundation';
 import { tokens } from './tokens/squad-oss';
 import { makeThemeStyles } from './styles';
 
-export const oss = makeTheme(makeThemeStyles(tokens));
+export const oss = makeTheme(makeThemeStyles({ ...foundation, ...tokens }));
